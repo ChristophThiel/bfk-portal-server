879bf51cf367ea43afb39ef4441de2dee141a8ec
ChristopThiel <christoph.thiel@liwest.at>
2018-07-08 14:26:23 +0200

    Create REST API

---
+++ BfkPortal.Web/Controllers/AuthentificationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BfkPortal.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthentificationController : Controller
+    {
+        [HttpGet]
+        [Route("test")]
+        public string Test()
+        {
+            return "This is a message!";
+        }
+    }
+}
