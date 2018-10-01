5f90a9bcd6f924d0a796af9b79fbb19bc75ba8d5
ChristophThiel <christoph.thiel@liwest.at>
2018-10-01 12:25:03 +0200

    Return memberships when login

--- BfkPortal.Web/Controllers/AuthenticationController.cs
+++ BfkPortal.Web/Controllers/AuthenticationController.cs
@@ -31,6 +31,10 @@ namespace BfkPortal.Web.Controllers
             if (!_service.ModelState.IsValid)
                 return BadRequest(ModelState);
 
+            var user = await _service.UnitOfWork.Users.FindAsync(userId);
+            await _service.UnitOfWork.Users.LoadCollectionAsync(user, nameof(user.Memberships));
+            foreach (var membership in user.Memberships)
+                await _service.UnitOfWork.Memberships.LoadReferenceAsync(membership, nameof(membership.Organisation));
             var result = new
             {
                 Token = await _service.CreateJsonWebTokenAsync(userId),
