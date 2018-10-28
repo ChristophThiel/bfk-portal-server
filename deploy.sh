0e94288aa727ee5f3e2d5d2a2e902edc73518f7e
ChristopThiel <christoph.thiel@liwest.at>
2018-10-28 11:04:20 +0100

    Combine login.sh and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
-sh login.sh
+#!/usr/bin/expect
+spawn heroku login
+expect "Email:*"
+send $DOCKER_USERNAME
+expect "Password:*"
+send $DOCKER_PASSWORD
 docker push registry.heroku.com/bfk-portal/web
 heroku container:push web -a bfk-portal
 heroku container:release web -a bfk-portal
