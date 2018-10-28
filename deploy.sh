6a35080f9737640cd78c3e2a76673cabc06d6df8
ChristopThiel <christoph.thiel@liwest.at>
2018-10-28 11:12:20 +0100

    Login into docker via auth token

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,2 @@
-#!/usr/bin/expect
-spawn heroku login
-expect "Email:*"
-send $DOCKER_USERNAME
-expect "Password:*"
-send $DOCKER_PASSWORD
-docker push registry.heroku.com/bfk-portal/web
-heroku container:push web -a bfk-portal
-heroku container:release web -a bfk-portal
+#!/bin/bash
+docker login --username
--- login.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/expect
-spawn heroku login
-expect "Email:*"
-send $DOCKER_USERNAME
-expect "Password:*"
-send $DOCKER_PASSWORD
