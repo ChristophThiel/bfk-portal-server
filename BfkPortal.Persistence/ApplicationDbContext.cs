2248e43d8586682627f0189d5b7b7c797b378871
ChristophThiel <christoph.thiel@liwest.at>
2019-02-23 20:53:27 +0100

    Fix CD to heroku

--- BfkPortal.Persistence/ApplicationDbContext.cs
+++ BfkPortal.Persistence/ApplicationDbContext.cs
@@ -6,56 +6,56 @@ using Microsoft.Extensions.Configuration;
 
 namespace BfkPortal.Persistence
 {
-  public class ApplicationDbContext : DbContext
-  {
-    public DbSet<User> Users { get; set; }
-
-    public DbSet<Preference> Preferences { get; set; }
-
-    public DbSet<Role> Roles { get; set; }
-
-    public DbSet<Appointment> Appointments { get; set; }
-
-    public DbSet<Offer> Offers { get; set; }
-
-    public DbSet<Organisation> Organisations { get; set; }
-
-    public DbSet<Core.Models.File> Files { get; set; }
-
-    public DbSet<Membership> Memberships { get; set; }
-
-    public DbSet<Participation> Participations { get; set; }
-
-    public DbSet<Entitlement> Entitlements { get; set; }
-
-    public DbSet<Possession> Possessions { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public class ApplicationDbContext : DbContext
     {
-      var configuration = new ConfigurationBuilder()
-          .SetBasePath(Environment.CurrentDirectory)
-          .AddJsonFile("appsettings.json")
-          .Build();
+        public DbSet<User> Users { get; set; }
 
-      if (configuration["Provider"] == "PostgreSql")
-      {
-        /*var connectionString = "postgres://vjxbjytwvvcngo:e97f16afdfc862a5b1eebcb0767d578bee970b6ecaa386d775d98d9fb83e1955@ec2-79-125-6-250.eu-west-1.compute.amazonaws.com:5432/darg2shjgsm2t6"; //System.Environment.GetEnvironmentVariable("DATABASE_URL");
-        if (Uri.TryCreate(connectionString, UriKind.Absolute, out var uri))
+        public DbSet<Preference> Preferences { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<Appointment> Appointments { get; set; }
+
+        public DbSet<Offer> Offers { get; set; }
+
+        public DbSet<Organisation> Organisations { get; set; }
+
+        public DbSet<Core.Models.File> Files { get; set; }
+
+        public DbSet<Membership> Memberships { get; set; }
+
+        public DbSet<Participation> Participations { get; set; }
+
+        public DbSet<Entitlement> Entitlements { get; set; }
+
+        public DbSet<Possession> Possessions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-          var userInfos = uri.UserInfo.Split(':');
-          var builder = new StringBuilder();
-          builder.Append("Server=").Append(uri.Host)
-              .Append(";Port=").Append(uri.Port)
-              .Append(";User Id=").Append(userInfos[0])
-              .Append(";Password=").Append(userInfos[1])
-              .Append(";Database=").Append(uri.LocalPath.Substring(1))
-              .Append(";SSLMode=Require;TrustServerCertificate=True");
-          optionsBuilder.UseNpgsql(builder.ToString());
-        }*/
-        optionsBuilder.UseNpgsql("User ID=service;Password=asdf;Host=localhost;Port=5432;Database=bfkportal;Pooling=true;");
-      }
-      else
-        optionsBuilder.UseSqlite(configuration.GetConnectionString("Sqlite"));
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            if (configuration["Provider"] == "PostgreSql")
+            {
+                var connectionString = "postgres://vjxbjytwvvcngo:e97f16afdfc862a5b1eebcb0767d578bee970b6ecaa386d775d98d9fb83e1955@ec2-79-125-6-250.eu-west-1.compute.amazonaws.com:5432/darg2shjgsm2t6"; //System.Environment.GetEnvironmentVariable("DATABASE_URL");
+                if (Uri.TryCreate(connectionString, UriKind.Absolute, out var uri))
+                {
+                    var userInfos = uri.UserInfo.Split(':');
+                    var builder = new StringBuilder();
+                    builder.Append("Server=").Append(uri.Host)
+                        .Append(";Port=").Append(uri.Port)
+                        .Append(";User Id=").Append(userInfos[0])
+                        .Append(";Password=").Append(userInfos[1])
+                        .Append(";Database=").Append(uri.LocalPath.Substring(1))
+                        .Append(";SSLMode=Require;TrustServerCertificate=True");
+                    optionsBuilder.UseNpgsql(builder.ToString());
+                }
+                // optionsBuilder.UseNpgsql("User ID=service;Password=asdf;Host=localhost;Port=5432;Database=bfkportal;Pooling=true;");
+            }
+            else
+                optionsBuilder.UseSqlite(configuration.GetConnectionString("Sqlite"));
+        }
     }
-  }
 }
