b20a2522872d4f2f7bc837a3714396bb05d6f6ef
ChristophThiel <christoph.thiel@liwest.at>
2019-03-12 13:08:42 +0100

    Remove changes from middleware and change order of connection string

--- BfkPortal.Persistence/ApplicationDbContext.cs
+++ BfkPortal.Persistence/ApplicationDbContext.cs
@@ -53,12 +53,12 @@ namespace BfkPortal.Persistence
             } */
             // DROPLET
             var builder = new StringBuilder();
-            builder.Append("User ID=")
+            builder.Append("Server=bfk-portal-postgres;Port=5432;Database=")
+                .Append(Environment.GetEnvironmentVariable("POSTGRES_DATABASE", EnvironmentVariableTarget.Machine))
+                .Append(";User Id=")
                 .Append(Environment.GetEnvironmentVariable("POSTGRES_USER", EnvironmentVariableTarget.Machine))
                 .Append(";Password=")
                 .Append(Environment.GetEnvironmentVariable("POSTGRES_PASSWORD", EnvironmentVariableTarget.Machine))
-                .Append(";Server=bfk-portal-postgres;Port=5432;Database=")
-                .Append(Environment.GetEnvironmentVariable("POSTGRES_DATABASE", EnvironmentVariableTarget.Machine))
                 .Append(";Pooling=true;");
             optionsBuilder.UseNpgsql(builder.ToString());
             // optionsBuilder.UseNpgsql("User ID=root;Password=S6qNqxjQJLE2HzWvfojriEtD4CjLMzgU;Host=207.154.201.108;Port=5432;Database=bfkportal;Pooling=true;");
